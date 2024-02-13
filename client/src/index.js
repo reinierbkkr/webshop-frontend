88363da6265df7a649679e81d492760cbce7213a
Reinier Bakker <rbakker@sogyo.nl>
2024-02-13 11:17:37 +0100

    store en voeg toe aan shopping basket

--- client/src/index.js
+++ client/src/index.js
@@ -4,12 +4,44 @@ function orderButtonClicked(event){
     const eventName = article.querySelector(".parkname").innerText;
     const nOfKids = Number(article.querySelector(".numberofadults").value);
     const nOfAdults = Number(article.querySelector(".numberofkids").value);
-    console.log(eventName)
-    console.log(nOfKids)
-    console.log(nOfAdults)
 
+    saveOrderInShoppingBasket(eventName, nOfKids, nOfAdults)
 
 }
+
+function saveOrderInShoppingBasket(eventName, nOfKids, nOfAdults){
+
+    const order = {
+        eventName: eventName,
+        nOfKids: nOfKids,
+        nOfAdults: nOfAdults
+    }
+
+    let cartItems = getShoppingBasket();
+    console.log(cartItems);
+    cartItems.push(order);
+
+    let cartItemsJson = JSON.stringify(cartItems);
+
+    localStorage.setItem("abc", cartItemsJson);
+
+
+
+
+    
+}
+
+function getShoppingBasket(){
+    const cartItemsJson = localStorage.getItem("abc");
+    if (cartItemsJson) {
+        return JSON.parse(cartItemsJson);
+    } else {
+        return [];
+    }
+}
+
+
+
 
 const buttons = document.querySelectorAll("button.orderbutton");
 
