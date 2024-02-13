d1c5f7b4692c2321cc66e014a1fae5a4d0ac1eab
Reinier Bakker <rbakker@sogyo.nl>
2024-02-13 10:24:40 +0100

    get naam en n of kids en adults na buttonclick

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,14 @@
 function orderButtonClicked(event){
     const button = event.target;
     const article = button.parentNode.parentNode;
-    const eventName = article.querySelector(".parkname").textContent;
-
+    const eventName = article.querySelector(".parkname").innerText;
+    const nOfKids = Number(article.querySelector(".numberofadults").value);
+    const nOfAdults = Number(article.querySelector(".numberofkids").value);
     console.log(eventName)
+    console.log(nOfKids)
+    console.log(nOfAdults)
+
+
 }
 
 const buttons = document.querySelectorAll("button.orderbutton");
